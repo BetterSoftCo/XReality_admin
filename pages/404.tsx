86e8f4cce17090cb1b4d1e31e7af58f2e9a64954
simaheidariesf <simaheidariesfahani@gmail.com>
2022-06-24 18:17:23 +0430

    add errors pages

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+//components
+
+import NotFound from '../components/Errors/NotFound'
+
+// pages/404.js
+export default function Custom404() {
+  return (
+    <>
+      <NotFound />
+    </>
+  )
+}
