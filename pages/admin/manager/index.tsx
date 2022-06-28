e0d3d07085ed30a0efc09268195c69218e045ef5
simaheidariesf <simaheidariesfahani@gmail.com>
2022-06-28 13:23:47 +0430

    add pages for admin

---
+++ pages/admin/manager/index.tsx
@@ -0,0 +1,17 @@
+import ManagerLayout from 'layouts/ManagerLayout'
+
+export default function AdminManager() {
+  return (
+    <>
+      <ManagerLayout>
+        <>
+          <div className="px-8">
+            <h2 className="text-2xl text-indigo-800 font-medium mb-2">
+              پنل مدیریتی مدیر
+            </h2>
+          </div>
+        </>
+      </ManagerLayout>
+    </>
+  )
+}
