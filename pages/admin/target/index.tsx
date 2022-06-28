d5359443b7c543a88cff9d735ad28bab9d62ab75
simaheidariesf <simaheidariesfahani@gmail.com>
2022-06-28 18:27:45 +0430

    fix targets list
    add pagination

--- pages/admin/target/index.tsx
+++ pages/admin/target/index.tsx
@@ -1,9 +1,6 @@
-import { PencilIcon, XIcon } from '@heroicons/react/outline'
 import AdminLayout from 'layouts/AdminLayout'
 import Link from 'next/link'
 import { useSession, getSession } from 'next-auth/react'
-import Swal from 'sweetalert2'
-import { useRouter } from 'next/router'
 import TargetItem from 'components/Target/TargetItem'
 import Pagination from 'components/Pagination'
 import { useState } from 'react'
@@ -21,78 +18,17 @@ type targetType = {
   type: string
 }
 
-
 const Targets = ({ targets }: any) => {
   const [currentPage, setCurrentPage] = useState(1)
   const pageSize = 9
 
-  const { data: session, status } = useSession()
-  const router = useRouter()
+  // const router = useRouter()
 
-  const handlePageChange = (page: number)=>{
+  const handlePageChange = (page: number) => {
     setCurrentPage(page)
   }
 
   const paginateTargets = paginate(targets, currentPage, pageSize)
-
-  const showAlert = (targetId: string) => {
-    Swal.fire({
-      title: 'حذف تارگت',
-      text: 'آیا اطمینان به حذف تارگت دارید؟',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#34d399',
-      cancelButtonColor: '#f87171',
-      confirmButtonText: 'آره مطمئنم',
-      cancelButtonText: 'حالا نه',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteTarget(targetId)
-        Swal.fire({
-          title: 'تارگت با موفقیت حذف شد',
-          icon: 'success',
-          confirmButtonText: 'حله',
-        })
-      }
-    })
-  }
-
-  const deleteMediaTarget = async (targetId: string) => {
-    const endpoint = `https://xrealityapi.sinamn75.com/api/Media/${targetId}`
-    const options = {
-      method: 'DELETE',
-      headers: {
-        Accept: '*/*',
-        Authorization: `${session?.accessToken}`,
-      },
-    }
-    const response = await fetch(endpoint, options)
-    console.log('response', response)
-
-    if (response.status == 200) {
-      router.push('/admin/target')
-    }
-  }
-
-  const deleteTarget = async (targetId: string) => {
-    //delete media first
-    deleteMediaTarget(targetId)
-    //delete data second
-    //TODO: DELETE MEDIA AND TARGET
-
-    // const endpoint = `https://xrealityapi.sinamn75.com/api/category/${targetId}`
-    // const options = {
-    //   method: 'DELETE',
-    //   headers: {
-    //     Accept: '*/*',
-    //     Authorization: `${session?.accessToken}`,
-    //   },
-    // }
-    // const response = await fetch(endpoint, options)
-    // if (response.status == 200) {
-    //   router.push('/admin/target')
-    // }
-  }
 
   return (
     <>
@@ -111,12 +47,17 @@ const Targets = ({ targets }: any) => {
                 </a>
               </Link>
             </div>
-             <div className="grid grid-cols-3 gap-x-3 gap-y-4">
+            <div className="grid grid-cols-3 gap-x-3 gap-y-4">
               {paginateTargets.map((target: targetType) => (
                 <TargetItem key={target.id} target={target} />
               ))}
             </div>
-            <Pagination items={targets.length} currentPage={currentPage} pageSize={pageSize} onPageChange={handlePageChange} /> 
+            <Pagination
+              items={targets.length}
+              currentPage={currentPage}
+              pageSize={pageSize}
+              onPageChange={handlePageChange}
+            />
           </div>
         )}
       </AdminLayout>
