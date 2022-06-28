b21a3e4c2c2cdf74521bdcef6b8bb1d97b7689ae
simaheidariesf <simaheidariesfahani@gmail.com>
2022-06-28 13:19:12 +0430

    fix sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -16,60 +16,77 @@ import Link from 'next/link'
 export function Sidebar() {
   const [open, setOpen] = useState(true)
   const [submenuOpen, setsubmenuOpen] = useState(false)
+  const [activeSubmenu, setActiveSubmenu] = useState(0)
+  const [activeColorMenu, setActiveColorMenu] = useState(0)
 
   const menus = [
     {
+      id: 1,
       title: 'داشبورد',
       icon: <HomeIcon className="w-5 text-gray-400" />,
       href: '/admin',
     },
     {
+      id: 2,
       title: 'پروفایل من',
       spacing: false,
       icon: <UserIcon className="w-5 text-gray-400" />,
       href: '/admin/profile',
     },
     {
+      id: 3,
       title: 'تارگت های من',
       submneu: true,
       submeunItems: [
-        { title: 'تارگت ها', href: '/admin/target' },
-        { title: 'افزودن تارگت', href: '/admin/target/add' },
+        { id: 1, title: 'تارگت ها', href: '/admin/target' },
+        { id: 2, title: 'افزودن تارگت', href: '/admin/target/add' },
       ],
       icon: <StarIcon className="w-5 text-gray-400" />,
     },
     {
+      id: 4,
       title: 'محتوا های من',
       submneu: true,
       submeunItems: [
-        { title: 'محتوا ها', href: '/admin/content' },
-        { title: 'افزودن محتوا', href: '/admin/content/add' },
+        { id: 1, title: 'محتوا ها', href: '/admin/content' },
+        { id: 2, title: 'افزودن محتوا', href: '/admin/content/add' },
       ],
       icon: <StarIcon className="w-5 text-gray-400" />,
     },
     {
+      id: 5,
+      title: 'کتابخانه XR',
+      submneu: false,
+      icon: <StarIcon className="w-5 text-gray-400" />,
+      href: '/admin/library',
+    },
+    {
+      id: 6,
       title: 'راهنما',
       spacing: false,
       icon: <QuestionMarkCircleIcon className="w-5 text-gray-400" />,
       href: '/admin/help',
     },
     {
+      id: 7,
       title: 'چت',
       spacing: true,
       submneu: true,
       submeunItems: [
-        { title: 'چت ها', href: '/admin/chat' },
-        { title: 'چت جدید', href: '/admin/chat/add' },
+        { id: 1, title: 'چت ها', href: '/admin/chat' },
+        { id: 2, title: 'چت جدید', href: '/admin/chat/add' },
       ],
       icon: <MailIcon className="w-5 text-gray-400" />,
     },
     {
+      id: 8,
       title: 'نسخه حرفه ای',
       spacing: true,
       icon: <StarIcon className="w-5 text-gray-400" />,
       href: '/upgrade',
     },
     {
+      id: 9,
       title: '',
       icon: (
         <LogoutIcon
@@ -92,8 +109,9 @@ export function Sidebar() {
             open ? 'w-64' : 'w-16'
           } duration-300 relative`}
         >
+          {/* TOGGLE ARROW */}
           <ArrowSmLeftIcon
-            className={`bg-white text-indigo-600 w-8 rounded-full absolute -left-8 top-5 border border-gray-200 cursor-pointer ${
+            className={`bg-white text-indigo-600 w-8 rounded-full absolute -left-8 top-5 border border-gray-200 cursor-pointer z-40 ${
               !open && 'rotate-y-180'
             }`}
             onClick={() => setOpen(!open)}
@@ -118,12 +136,14 @@ export function Sidebar() {
           </div>
 
           <ul className="pt-2">
-            {menus.map((menu, index) => (
+            {menus.map((menu: any) => (
               <>
                 <li
-                  key={index}
-                  className={`text-gray-300 text-lg flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-100 hover:text-white rounded-md mt-2 ${
-                    menu.spacing ? 'mt-4' : 'mt-1'
+                  key={menu.id}
+                  className={`text-gray-300 text-lg flex items-center gap-x-4 cursor-pointer p-2 rounded-md mt-2 ${
+                    menu.spacing ? 'mt-4 ' : 'mt-1'
+                  } ${
+                    activeColorMenu == menu.id ? 'bg-gray-300' : 'bg-gray-100'
                   }`}
                 >
                   <span className="text-lg block float-right">
@@ -139,32 +159,49 @@ export function Sidebar() {
                     }`}
                   >
                     <Link href={menu.href ? menu.href : ''} passHref>
-                      <a> {menu.title}</a>
+                      <a
+                        onClick={() => {
+                          setsubmenuOpen(!submenuOpen)
+                          setActiveSubmenu(menu.id)
+                          setActiveColorMenu(menu.id)
+                        }}
+                      >
+                        {menu.title}
+                      </a>
                     </Link>
                   </span>
                   {menu.submneu && open && (
                     <ChevronDownIcon
                       className={`w-4 text-gray-500 ${
-                        submenuOpen && 'rotate-z-180'
+                        submenuOpen &&
+                        activeSubmenu == menu.id &&
+                        'rotate-z-180'
                       }`}
-                      onClick={() => setsubmenuOpen(!submenuOpen)}
+                      onClick={() => {
+                        setsubmenuOpen(!submenuOpen)
+                        setActiveSubmenu(menu.id)
+                        setActiveColorMenu(menu.id)
+                      }}
                     />
                   )}
                 </li>
-                {menu.submneu && submenuOpen && open && (
-                  <ul>
-                    {menu.submeunItems.map((submneuItem, index) => (
-                      <li
-                        key={index}
-                        className="text-gray-500 text-lg flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-gray-300 rounded-md"
-                      >
-                        <Link href={submneuItem.href} passHref>
-                          <a>{submneuItem.title}</a>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                )}
+                {menu.submneu &&
+                  submenuOpen &&
+                  open &&
+                  activeSubmenu == menu.id && (
+                    <ul>
+                      {menu.submeunItems.map((submneuItem: any) => (
+                        <li
+                          key={submneuItem.id}
+                          className="text-gray-500 text-lg flex items-center gap-x-4 cursor-pointer p-2 px-5 rounded-md"
+                        >
+                          <Link href={submneuItem.href} passHref>
+                            <a>{submneuItem.title}</a>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
               </>
             ))}
           </ul>
